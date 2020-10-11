4839ba23bb6891fd0b988d46c8630bcd13a89646
JCJ <jesse.jackson.c@gmail.com>
2020-10-11 17:37:47 -0500

    Creating array objects for easier access into inquirer

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 let inquirer = require("inquirer");
 let server = require("./db-server");
+
+const viewOptions = [
+  "View Departments",
+  "View Roles",
+  "View Employees",
+  "Update Employee",
+  "exit",
+];
+
+const employeeOptions = [
+  "Michael Scott",
+  "Jim Halpert",
+  "Dwight Schrute",
+  "Pam Beesly",
+  "Kevin Malone",
+  "Darryl Philbin",
+  "exit",
+];
+
+const updateOptions = ["First Name", "Last Name", "Role", "exit"];
