157f3102bb971d04af9bb892acc8913e7aaeeb7d
JCJ <jesse.jackson.c@gmail.com>
2020-10-11 17:40:29 -0500

    Calling function that will ask user initial question. Break cases included depending on input

--- index.js
+++ index.js
@@ -20,3 +20,37 @@ const employeeOptions = [
 ];
 
 const updateOptions = ["First Name", "Last Name", "Role", "exit"];
+
+function initialInquiry() {
+  inquirer
+    .prompt({
+      name: "inquiry",
+      type: "list",
+      message: "What would you like to do?",
+      choices: viewOptions,
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case viewOptions[0]:
+          departmentView();
+          break;
+
+        case viewOptions[1]:
+          roleView();
+          break;
+
+        case viewOptions[2]:
+          employeeView();
+          break;
+
+        case viewOptions[3]:
+          updateEmployee();
+
+        case updateOptions[4]:
+          connection.end();
+          break;
+      }
+    });
+}
+
+initialInquiry();
